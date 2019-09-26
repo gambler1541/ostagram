2e587df57887e84c9a78af5f99ef946cd0bf2163
pmb <gambler1541@gmail.com>
2019-09-26 12:42:02 +0900

    login,logout template, url

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('login/', auth_view.LoginView.as_view(), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 
 ]
