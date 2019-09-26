ec6b49bd8e44aa76445ff8e87dc1b5c53a25ff06
pmb <gambler1541@gmail.com>
2019-09-26 12:02:43 +0900

    url - static 구문

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +23,6 @@ urlpatterns = [
     path('', include('photo.urls')),
 
 ]
+
+# 이 구문은 debug=true일 경우에만 실행
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
